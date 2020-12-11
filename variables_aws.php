b54710d1d5f079e1ea4e6c67a9001f03c67c3615
Alex Oficina <github@mycodes4you.com>
2020-12-11 17:01:32 -0600

    Reparando branch

---
+++ variables_aws.php
@@ -0,0 +1,15 @@
+<?php
+// --- Variables para utilizar los servicios Amazon Web Services --
+
+// --- Definimos el bucket donde se subira el archivo
+$bucket = 'fjro9e2fe3la1mjp6hs0';
+
+// --- Se definen las llaves de acceso y la region --
+$aws_key = 'AKIAQI7O6OJN5ZK4B7TM';
+$aws_secret = '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';
+$aws_region = 'us-east-1';
+
+// --- Definición del Web Access Point para el bucket --
+$aws_web_ap = 'fjro9e2fe3la1mjp6hs0.s3';
+
+?>
